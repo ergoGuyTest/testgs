85d51ef0e02977f2b5f18f4577ddb18c8584371d
ergoGuyTest <joshua.schroeder@cnh.com>
2025-12-04 15:53:08 -0600

    Add files via upload

---
+++ my.js
@@ -0,0 +1,39 @@
+
+function testGS(){
+
+    const url = "https://script.google.com/macros/s/AKfycbw96ReVvu3F3F6VByIox61LO9uWqwYuSPx8vch_B5vGZi1jI6TXCEqNnIBBdpS_7R0XYg/exec";
+
+    fetch(url)
+        .then(d => d.json())
+        .then(d => {
+    
+               document.getElementById("app").textContent = d[0].status;
+    
+        });
+
+}
+
+function addGS(){
+
+    const url = "https://script.google.com/macros/s/AKfycbw96ReVvu3F3F6VByIox61LO9uWqwYuSPx8vch_B5vGZi1jI6TXCEqNnIBBdpS_7R0XYg/exec";
+
+    fetch(url,{
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        //credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+            //not required!
+        },
+        redirect: 'follow',
+        //referrerPolicy: 'no-referrer',
+        body: JSON.stringify({name:"Ileana"})
+    });
+
+}
+
+document.getElementById("btn2").addEventListener("click",addGS);
+
+
+document.getElementById("btn").addEventListener("click",testGS);
