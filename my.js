ca35bd98b5bb446d9fb8b6499d6377556831586e
ergoGuyTest <joshua.schroeder@cnh.com>
2025-12-04 16:35:35 -0600

    Update my.js

--- my.js
+++ my.js
@@ -28,7 +28,7 @@ function addGS(){
         },
         redirect: 'follow',
         //referrerPolicy: 'no-referrer',
-        body: JSON.stringify({name:"Ileana"})
+        body: JSON.stringify({first:"Michael",last:"Jordan",phone:"312-006-1998"})
     });
 
 }
@@ -37,3 +37,4 @@ document.getElementById("btn2").addEventListener("click",addGS);
 
 
 document.getElementById("btn").addEventListener("click",testGS);
+
